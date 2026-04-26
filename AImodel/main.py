741f27067e9c15577cbefed3b327ba21b6ce94c7
NchangRoy <royfru44@gmail.com>
2026-04-26 02:57:14 +0100

    TWIST_1: 59 claims arrivent en meme temps et seulement trois verifcateurs sont disponibles. Dans ce cas,il est tres dificile de traiter tous les claims commes le nombre de verificateurs est petit. Alors, on cree un file de priorite dependant du theme general du claim par exemple. Notre agent et equipe avec un outil classify_and_rank qui tire le theme general d'un claim et la done une priorite predefini

--- AImodel/main.py
+++ AImodel/main.py
@@ -38,14 +38,14 @@ Remove contradictory claims and return only consistent ones.
 @app.post("/score_claim")
 def score_claim(payload: dict):
 
-    result = run_agent({
+    result2 = run_agent({
         "claim": payload["claim"],
         "evidences": payload["evidences"],
         "claim_evidences": payload["claim_evidences"],
         "rules": payload["rules"]
     })
 
-    return result
+    return result2
 
 
 @app.post("/classify_claims")
